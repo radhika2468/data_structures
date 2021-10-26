8848f2a65b0794254ab916e2b8b812ebe45f3459
Radhika <248radhika@gmail.com>
2021-10-26 10:04:09 +0530

    ice_cream_parlor

---
+++ searching/Ice_cream_parlor.java
@@ -0,0 +1,78 @@
+//Question-https://www.hackerrank.com/challenges/icecream-parlor/problem?isFullScreen=false
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'icecreamParlor' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER m
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> icecreamParlor(int m, List<Integer> arr) {
+    // Write your code here
+   List<Integer> k = new ArrayList<Integer>(2);
+    for(int i=0;i<arr.size();i++)
+    {
+        for(int j=i+1;j<arr.size();j++)
+        {
+           if(arr.get(i)+arr.get(j)==m)
+           {
+               k.add(i+1);
+               k.add(j+1);
+               return k;
+           }
+        }
+    }
+    return k;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arr = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrTemp[i]);
+                arr.add(arrItem);
+            }
+
+            List<Integer> result = Result.icecreamParlor(m, arr);
+
+            for (int i = 0; i < result.size(); i++) {
+                bufferedWriter.write(String.valueOf(result.get(i)));
+
+                if (i != result.size() - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
